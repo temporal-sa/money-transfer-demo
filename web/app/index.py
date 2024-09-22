49a349af233abff5087397dd5e2e21c1ca96811c
Mike Nichols <mike.nichols@temporal.io>
2024-09-22 19:36:37 -0400

    support app_info and access in view

--- web/app/index.py
+++ web/app/index.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+app_info = {
+    'name': 'Temporal Money Transfer',
+    'temporal': {
+        'namespace': 'foo'
+    }
+}
+@app.before_request
+def apply_app_info():
+    g.app_info = app_info
+
+@app.context_processor
+def view_app_info():
+    return dict(app_info=g.app_info)
 
 @app.route('/layout')
 def layout():
-    app_info = {
-        'name': 'Temporal Money TransferDude',
-        'temporal': {
-            'namespace': 'foo'
-        }
-    }
-    return render_template(template_name_or_list='layout.html', app_info = app_info)
+    return render_template(template_name_or_list='layout.html')
 
 @app.route('/')
 def index():
